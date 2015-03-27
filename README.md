5a2ca76785b88f9337d349718c0bf04dd752684d
matti16 <bmattia92@gmail.com>
2015-03-27 20:48:23 +0100

    fix in the max value

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # GammaCorrection
 
 Use MPI (+OpenMP) to implement a parallel version of the gamma correction image enhancement algorithm for grayscale images. Use the image format you prefer ("pgm - portable graymap" is an easy to parse grayscale format). The code has to be demonstrated using at least two physical machines connected in a LAN (with or without additional virtual machines to emulate a larger cluster).
+
+
+Compile with ```mpicc```:
+
+```mpicc -fopenmp -std=c99 *.c -lm```
+
+
+Run with ```mpirun```:
+
+```mpirun -np 4 a.out ../images/sample.pgm 1 3```
--- code/gamma.c
+++ code/gamma.c
@@ -1,15 +1,28 @@
 #include "../headers/gamma.h"
 
+#define MAX 255
+
 void gamma_correct(int* pixels, int size, float a, float gamma){
-	int i;
+	int i, pixel;
 	#pragma omp parallel for private(i)
 	for (i=0; i<size; i++){
-		pixels[i] = (int) ( a * ( pow( (double)pixels[i], (double)gamma )));
+		pixel = (int) ( a * ( pow( (double)pixels[i], (double)gamma )));
+		if(pixel > MAX){
+			pixels[i] = MAX;
+		}else{
+			pixels[i] = pixel;
+		}
 	}
 }
 
 void single_gamma(int* value, float a, float gamma){
-	*(value) = (int) ( a * ( pow( (double)(*(value)), (double)gamma ))) ;
+	int max;
+	max = (int) ( a * ( pow( (double)(*(value)), (double)gamma )));
+	if(max > MAX){
+		*(value) = MAX;
+	}else{
+		*(value) = max;
+	}
 }	
 
 
